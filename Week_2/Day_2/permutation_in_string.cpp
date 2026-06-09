1b0f82eb75f7587fdbca42ea4e756d06e4930e91
nandiniAIx <n4567491@gmail.com>
2026-06-09 21:43:02 +0530

    add file

---
+++ Week_2/Day_2/permutation_in_string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+    sort(s1.begin(), s1.end());
+    for (int i =0; i <= s2.length(); i++){
+        string temp = s2.substr(i,s1.length());
+        sort(temp.begin(), temp.end());
+        if (temp == s1){
+            return true;
+        }
+    }
+    return false;
+    }
+};
